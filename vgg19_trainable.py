16c6271ca0c7a7c5d75af02f49655c9a50a301d2
Guojun Yang <GYang44@users.noreply.github.com>
2019-04-27 00:20:09 -0500

    adjusted filter size, replace concatinate operation with addition operation

--- vgg19_trainable.py
+++ vgg19_trainable.py
@@ -42,30 +42,30 @@ class Vgg19:
         ])
         assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
-        self.conv1_1 = self.conv_layer(bgr, 3, 64, "conv1_1")
-        self.conv1_2 = self.conv_layer(self.conv1_1, 64, 64, "conv1_2")
+        self.conv1_1 = self.conv_layer(bgr, 3, 3, 64, "conv1_1")
+        self.conv1_2 = self.conv_layer(self.conv1_1, 3, 64, 64, "conv1_2")
         self.pool1 = self.max_pool(self.conv1_2, 'pool1')
 
-        self.conv2_1 = self.conv_layer(self.pool1, 64, 128, "conv2_1")
-        self.conv2_2 = self.conv_layer(self.conv2_1, 128, 128, "conv2_2")
+        self.conv2_1 = self.conv_layer(self.pool1, 3, 64, 128, "conv2_1")
+        self.conv2_2 = self.conv_layer(self.conv2_1, 3, 128, 128, "conv2_2")
         self.pool2 = self.max_pool(self.conv2_2, 'pool2')
 
-        self.conv3_1 = self.conv_layer(self.pool2, 128, 256, "conv3_1")
-        self.conv3_2 = self.conv_layer(self.conv3_1, 256, 256, "conv3_2")
-        self.conv3_3 = self.conv_layer(self.conv3_2, 256, 256, "conv3_3")
-        self.conv3_4 = self.conv_layer(self.conv3_3, 256, 256, "conv3_4")
+        self.conv3_1 = self.conv_layer(self.pool2, 3, 128, 256, "conv3_1")
+        self.conv3_2 = self.conv_layer(self.conv3_1, 3, 256, 256, "conv3_2")
+        self.conv3_3 = self.conv_layer(self.conv3_2, 3, 256, 256, "conv3_3")
+        self.conv3_4 = self.conv_layer(self.conv3_3, 3, 256, 256, "conv3_4")
         self.pool3 = self.max_pool(self.conv3_4, 'pool3')
 
-        self.conv4_1 = self.conv_layer(self.pool3, 256, 512, "conv4_1")
-        self.conv4_2 = self.conv_layer(self.conv4_1, 512, 512, "conv4_2")
-        self.conv4_3 = self.conv_layer(self.conv4_2, 512, 512, "conv4_3")
-        self.conv4_4 = self.conv_layer(self.conv4_3, 512, 512, "conv4_4")
+        self.conv4_1 = self.conv_layer(self.pool3, 3, 256, 512, "conv4_1")
+        self.conv4_2 = self.conv_layer(self.conv4_1, 3, 512, 512, "conv4_2")
+        self.conv4_3 = self.conv_layer(self.conv4_2, 3, 512, 512, "conv4_3")
+        self.conv4_4 = self.conv_layer(self.conv4_3, 3, 512, 512, "conv4_4")
         self.pool4 = self.max_pool(self.conv4_4, 'pool4')
 
-        self.conv5_1 = self.conv_layer(self.pool4, 512, 512, "conv5_1")
-        self.conv5_2 = self.conv_layer(self.conv5_1, 512, 512, "conv5_2")
-        self.conv5_3 = self.conv_layer(self.conv5_2, 512, 512, "conv5_3")
-        self.conv5_4 = self.conv_layer(self.conv5_3, 512, 512, "conv5_4")
+        self.conv5_1 = self.conv_layer(self.pool4, 3, 512, 512, "conv5_1")
+        self.conv5_2 = self.conv_layer(self.conv5_1, 3, 512, 512, "conv5_2")
+        self.conv5_3 = self.conv_layer(self.conv5_2, 3, 512, 512, "conv5_3")
+        self.conv5_4 = self.conv_layer(self.conv5_3, 3, 512, 512, "conv5_4")
         self.pool5 = self.max_pool(self.conv5_4, 'pool5')
 
         self.fc6 = self.fc_layer(self.pool5, 25088, 4096, "fc6")  # 25088 = ((224 // (2 ** 5)) ** 2) * 512
@@ -75,31 +75,37 @@ class Vgg19:
         elif self.trainable:
             self.relu6 = tf.nn.dropout(self.relu6, self.dropout)        
 
-        self.fc7 = self.fc_layer(self.fc6, 4096, 12544, "fc7_1") #12544 = 512*7*7/2
-        self.tn5_7 = self.conv_layer(self.pool5, 512, 256, "tn5_7_1")
-        self.conv7 = self.concat(self.fc7, self.tn5_7, "conv7")
-        self.upool7 = self.un_pool(self.conv7, "upool7")
-        
-        self.conv8_1 = self.conv_layer(self.upool7, 512, 256, "conv8_1")
-        self.tn4_8 = self.conv_layer(self.pool4, 512, 256, "tn4_8")
-        self.conv8_2 = self.concat(self.conv8_1, self.tn4_8, "conv8_2")
-        self.upool8 = self.un_pool(self.conv8_2, "upool8")
+        self.fc7_1 = self.fc_layer(self.fc6, 4096, 25088, "fc7_1") #12544 = 512*7*7
+        self.fc7_2 = tf.reshape(self.fc7_1, self.pool5.get_shape())
+        self.tn5_7_1 = self.conv_layer(self.pool5, 3, 512, 256, "tn5_7_1")
+        self.tn5_7_2 = self.conv_layer(self.tn5_7_1, 3, 256, 512, "tn5_7_2")
+        self.conv7_1 = self.sum(self.fc7_2, self.tn5_7_2, "conv7_1")
+        self.upool7 = self.un_pool(self.conv7_1, "upool7")        
+        self.conv7_2 = self.conv_layer(self.upool7, 3, 512, 512, "conv7_2")
 
-        self.conv9_1 = self.conv_layer(self.upool8, 512, 128, "conv9_1")
-        self.tn3_9 = self.conv_layer(self.pool3, 256, 128, "tn3_9")
-        self.conv9_2 = self.concat(self.conv9_1, self.tn3_9, "conv9_2")
-        self.upool9 = self.un_pool(self.conv9_2, "upool9")
+        self.tn4_8_1 = self.conv_layer(self.pool4, 3, 512, 256, "tn4_8_1")
+        self.tn4_8_2 = self.conv_layer(self.tn4_8_1, 3, 256, 512, "tn4_8_2")
+        self.conv8_1 = self.sum(self.conv7_2, self.tn4_8_2, "conv8_1")
+        self.upool8 = self.un_pool(self.conv8_1, "upool8")
+        self.conv8_2 = self.conv_layer(self.upool8, 3, 512, 256, "conv8_2")
 
-        self.conv10_1 = self.conv_layer(self.upool9, 256, 64, "conv10_1")
-        self.tn2_10 = self.conv_layer(self.pool2, 128, 64, "tn2_10")
-        self.conv10_2 = self.concat(self.conv10_1, self.tn2_10, "conv10_2")
-        self.upool10 = self.un_pool(self.conv10_2, "upool10")
+        self.tn3_9_1 = self.conv_layer(self.pool3, 3, 256, 128, "tn3_9_1")
+        self.tn3_9_2 = self.conv_layer(self.tn3_9_1, 3, 128, 256, "tn3_9_2")
+        self.conv9_1 = self.sum(self.conv8_2, self.tn3_9_2, "conv9_1")
+        self.upool9 = self.un_pool(self.conv9_1, "upool9")
+        self.conv9_2  = self.conv_layer(self.upool9, 3, 256, 128, "conv9_2")
 
-        self.conv11_1 = self.conv_layer(self.upool10, 128, 32, "conv11_1")
-        self.tn1_11 = self.conv_layer(self.pool1, 64, 32, "tn1_11")
-        self.conv11_2 = self.concat(self.conv11_1, self.tn1_11, "conv11_2")
-        self.upool10 = self.un_pool(self.conv11_2, "upool10")
-        
+        self.tn2_10_1 = self.conv_layer(self.pool2, 5, 128, 64, "tn2_10_1")
+        self.tn2_10_2 = self.conv_layer(self.tn2_10_1, 5, 64, 128, "tn2_10_2")
+        self.conv10_1 = self.sum(self.conv9_2, self.tn2_10_2, "conv10_1")
+        self.upool10 = self.un_pool(self.conv10_1, "upool10")
+        self.conv10_2 = self.conv_layer(self.upool10, 3, 128, 64, "conv10_2")
+
+        self.tn1_11_1 = self.conv_layer(self.pool1, 5, 64, 32, "tn1_11_1")
+        self.tn1_11_2 = self.conv_layer(self.tn1_11_1, 5, 32, 64, "tn1_11_2")
+        self.conv11_1 = self.sum(self.conv10_2, self.tn1_11_2, "conv11_1")
+        self.upool11 = self.un_pool(self.conv11_1, "upool11")
+        self.conv11_2 = self.conv_layer(self.upool11, 3, 64, 3, "conv11_2")        
 
         self.fc7 = self.fc_layer(self.relu6, 4096, 4096, "fc7")
         self.relu7 = tf.nn.relu(self.fc7)
@@ -131,9 +137,9 @@ class Vgg19:
             out = tf.reshape(out, out_size, name=name)
         return out
 
-    def conv_layer(self, bottom, in_channels, out_channels, name):
+    def conv_layer(self, bottom, filter_size, in_channels, out_channels, name):
         with tf.variable_scope(name):
-            filt, conv_biases = self.get_conv_var(3, in_channels, out_channels, name)
+            filt, conv_biases = self.get_conv_var(filter_size, in_channels, out_channels, name)
             conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
             bias = tf.nn.bias_add(conv, conv_biases)
             relu = tf.nn.relu(bias)
@@ -142,9 +148,6 @@ class Vgg19:
 
     def concat(self, leftNet, rightNet, name):
         with tf.variable_scope(name):
-            #TODO assert size are the same
-            #print(leftNet.get_shape())
-            #print(rightNet.get_shape())
             sameShape = False
             if (leftNet.shape.rank == rightNet.shape.rank) :
                 sameShape = True
@@ -153,10 +156,23 @@ class Vgg19:
                         sameShape = False
                         break
 
-            if not sameShape:
-                leftNet = tf.reshape(leftNet, rightNet.get_shape())
+            assert sameShape
             concat = tf.concat([leftNet,rightNet],3)
             return concat
+
+    def sum(self, leftNet, rightNet, name):
+        with tf.variable_scope(name):
+            sameShape = False
+            if (leftNet.shape.rank == rightNet.shape.rank) :
+                sameShape = True
+                for i in range(0,leftNet.shape.rank):
+                    if leftNet.shape.dims[i].value != rightNet.shape.dims[i].value:
+                        sameShape = False
+                        break
+
+            assert sameShape
+            sum = tf.nn.relu6(leftNet + rightNet)
+            return sum
 
     def fc_layer(self, bottom, in_size, out_size, name):
         with tf.variable_scope(name):
