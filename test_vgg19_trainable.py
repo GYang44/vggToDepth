16bfb62fe8a7902719dba0d52818efce8865a504
Guojun Yang <GYang44@users.noreply.github.com>
2019-06-02 23:31:18 -0500

    implemented weight store and recover.

--- test_vgg19_trainable.py
+++ test_vgg19_trainable.py
@@ -2,25 +2,23 @@
 Simple tester for the vgg19_trainable
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
 import sys
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import ops
 
-from DepthNet_MOD import Vgg19
-#from vgg19_trainable import Vgg19
+from vgg19_trainable import Vgg19
 import utils
 
-import numpy as np
-
-from tensorflow.python.framework import ops
+from time import time
 
 ops.reset_default_graph()
 
 # create list of directory to color inputs and depth outputs
-
 #dataSet = utils.DataHandler('/media/gyang/INO1/Archive/DepthTraining/depthPhoto', [224,224], 10)
-dataSet = utils.DataHandler('D:\\Archive\\DepthTraining\\depthPhoto', [224,224], 10)
+dataSet = utils.DataHandler('D:\\Archive\\DepthTraining\\depthPhoto', [224,224], 20)
 
-cycles = 10000
+cycles = 2000
 isTraining = True
 
 with tf.device('/gpu:0'):
@@ -30,9 +28,10 @@ with tf.device('/gpu:0'):
     images = tf.placeholder(tf.float32, [None, 224, 224, 3])
     true_out = tf.placeholder(tf.float32, [None, 224, 224, 1])
     train_mode = tf.placeholder(tf.bool)
-
+    resume = tf.placeholder(tf.bool)
+    
     vgg = Vgg19('./vgg19.npy')
-    vgg.build(images, train_mode)
+    vgg.build(images, train_mode = True, resume = False)
 
     print(vgg.get_var_count())
 
@@ -46,21 +45,21 @@ with tf.device('/gpu:0'):
         dataSet.reBatch()
         costAvg[i] = 0
         while dataSet.nextBatch():
-            inputImage, true_depth = dataSet.getBatch()
-    
+            inputImage, true_depth = dataSet.getBatch()            
             #load input and output image
-            _, costValue = sess.run([train, cost], feed_dict={images: inputImage, true_out: true_depth, train_mode: isTraining})
-            param = sess.run(vgg.var_dict)
-            print('Cycle: {}, cost value: {}'.format(i, costValue) )
+            _, costValue = sess.run([train, cost], feed_dict={images: inputImage, true_out: true_depth, train_mode: True, resume: False})
+            #param = sess.run(vgg.var_dict)
             costAvg[i] = costAvg[i] + costValue
         costAvg[i] = costAvg[i]/dataSet.getBatchCount()
+        print("Cycle: {}, Cost: {}".format(i, costAvg[i]))
     """
     #test trining result
     depth = sess.run(vgg.depth, feed_dict={images: inputImage, train_mode: False})
     for i in range(0, inputImage.shape[0]):
         utils.show_image(depth[i].reshape(224,224))
     """
-    #np.save('./cost.npy', costAvg)
+    np.save('./cost.npy', costAvg)
     # test save
-    #vgg.save_npy(sess, './test-save.npy')
+    vgg.save_npy(sess, './test-save.npy')
     sess.close()
+    
--- vgg19_trainable.py
+++ vgg19_trainable.py
@@ -18,9 +18,10 @@ class Vgg19:
 
         self.var_dict = {}
         self.trainable = trainable
+        self.resuem = not self.trainable
         self.dropout = dropout
 
-    def build(self, rgb, train_mode=None):
+    def build(self, rgb, train_mode=False, resume=True):
         """
         load variable from npy to build the VGG
 
@@ -42,7 +43,8 @@ class Vgg19:
         ])
         assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
-        self.trainable = (train_mode is not None) & False
+        self.trainable = train_mode and False
+        self.resume = resume or True
 
         self.conv1_1 = self.conv_layer(bgr, 3, 3, 64, "conv1_1")
         self.conv1_2 = self.conv_layer(self.conv1_1, 3, 64, 64, "conv1_2")
@@ -70,63 +72,44 @@ class Vgg19:
         self.conv5_4 = self.conv_layer(self.conv5_3, 3, 512, 512, "conv5_4")
         self.pool5 = self.max_pool(self.conv5_4, 'pool5')
 
+        self.trainable = train_mode
+        self.resume = resume
+
         self.fc6 = self.fc_layer(self.pool5, 25088, 4096, "fc6")  # 25088 = ((224 // (2 ** 5)) ** 2) * 512
-        self.relu6 = tf.nn.relu(self.fc6)
-        if train_mode is not None:
-            self.relu6 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu6, self.dropout), lambda: self.relu6)
-        elif self.trainable:
-            self.relu6 = tf.nn.dropout(self.relu6, self.dropout) 
 
-        self.trainable = (train_mode is not None) & True
-
-        self.fc7_1 = self.fc_layer(self.fc6, 4096, 25088, "fc7_1") #12544 = 512*7*7
-        #self.fc7_2 = tf.reshape(self.fc7_1, self.pool5.get_shape())
+        self.fc7_1 = self.fc_layer(self.fc6, 4096, 12544, "fc7_1") #12544 = 256*7*7
         self.tn5_7_1 = self.conv_layer(self.pool5, 3, 512, 256, "tn5_7_1")
-        self.tn5_7_2 = self.conv_layer(self.tn5_7_1, 3, 256, 512, "tn5_7_2")
-        self.conv7_1 = self.sum(tf.reshape(self.fc7_1, [tf.shape(self.pool5)[0],7,7,512]), self.tn5_7_2, "conv7_1")
+        self.tn5_7_2 = self.conv_layer(self.tn5_7_1, 3, 256, 256, "tn5_7_2")
+        self.conv7_1 = self.concat(tf.reshape(self.fc7_1, [tf.shape(self.pool5)[0],7,7,256]), self.tn5_7_2, "conv7_1")
         self.upool7 = self.un_pool(self.conv7_1, "upool7")        
-        self.conv7_2 = self.conv_layer(self.upool7, 3, 512, 512, "conv7_2")
+        self.conv7_2 = self.conv_layer(self.upool7, 3, 512, 256, "conv7_2")
 
         self.tn4_8_1 = self.conv_layer(self.pool4, 3, 512, 256, "tn4_8_1")
-        self.tn4_8_2 = self.conv_layer(self.tn4_8_1, 3, 256, 512, "tn4_8_2")
-        self.conv8_1 = self.sum(self.conv7_2, self.tn4_8_2, "conv8_1")
+        self.tn4_8_2 = self.conv_layer(self.tn4_8_1, 3, 256, 256, "tn4_8_2")
+        self.conv8_1 = self.concat(self.conv7_2, self.tn4_8_2, "conv8_1")
         self.upool8 = self.un_pool(self.conv8_1, "upool8")
-        self.conv8_2 = self.conv_layer(self.upool8, 3, 512, 256, "conv8_2")
+        self.conv8_2 = self.conv_layer(self.upool8, 3, 512, 128, "conv8_2")
 
         self.tn3_9_1 = self.conv_layer(self.pool3, 3, 256, 128, "tn3_9_1")
-        self.tn3_9_2 = self.conv_layer(self.tn3_9_1, 3, 128, 256, "tn3_9_2")
-        self.conv9_1 = self.sum(self.conv8_2, self.tn3_9_2, "conv9_1")
+        self.tn3_9_2 = self.conv_layer(self.tn3_9_1, 3, 128, 128, "tn3_9_2")
+        self.conv9_1 = self.concat(self.conv8_2, self.tn3_9_2, "conv9_1")
         self.upool9 = self.un_pool(self.conv9_1, "upool9")
-        self.conv9_2  = self.conv_layer(self.upool9, 3, 256, 128, "conv9_2")
+        self.conv9_2  = self.conv_layer(self.upool9, 3, 256, 64, "conv9_2")
 
         self.tn2_10_1 = self.conv_layer(self.pool2, 5, 128, 64, "tn2_10_1")
-        self.tn2_10_2 = self.conv_layer(self.tn2_10_1, 5, 64, 128, "tn2_10_2")
-        self.conv10_1 = self.sum(self.conv9_2, self.tn2_10_2, "conv10_1")
+        self.tn2_10_2 = self.conv_layer(self.tn2_10_1, 5, 64, 64, "tn2_10_2")
+        self.conv10_1 = self.concat(self.conv9_2, self.tn2_10_2, "conv10_1")
         self.upool10 = self.un_pool(self.conv10_1, "upool10")
-        self.conv10_2 = self.conv_layer(self.upool10, 3, 128, 64, "conv10_2")
+        self.conv10_2 = self.conv_layer(self.upool10, 3, 128, 32, "conv10_2")
 
         self.tn1_11_1 = self.conv_layer(self.pool1, 5, 64, 32, "tn1_11_1")
-        self.tn1_11_2 = self.conv_layer(self.tn1_11_1, 5, 32, 64, "tn1_11_2")
-        self.conv11_1 = self.sum(self.conv10_2, self.tn1_11_2, "conv11_1")
+        self.tn1_11_2 = self.conv_layer(self.tn1_11_1, 5, 32, 32, "tn1_11_2")
+        self.conv11_1 = self.concat(self.conv10_2, self.tn1_11_2, "conv11_1")
         self.upool11 = self.un_pool(self.conv11_1, "upool11")
         self.conv11_2 = self.conv_layer(self.upool11, 3, 64, 3, "conv11_2")
 
         self.depth = self.conv_layer(self.conv11_2, 3, 3, 1, "depth")
         pass
-        """
-        self.fc7 = self.fc_layer(self.relu6, 4096, 4096, "fc7")
-        self.relu7 = tf.nn.relu(self.fc7)
-        if train_mode is not None:
-            self.relu7 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu7, self.dropout), lambda: self.relu7)
-        elif self.trainable:
-            self.relu7 = tf.nn.dropout(self.relu7, self.dropout)
-
-        self.fc8 = self.fc_layer(self.relu7, 4096, 1000, "fc8")
-
-        self.prob = tf.nn.softmax(self.fc8, name="prob")
-
-        self.data_dict = None
-        """
 
     def avg_pool(self, bottom, name):
         return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
@@ -210,7 +193,7 @@ class Vgg19:
         return weights, biases
 
     def get_var(self, initial_value, name, idx, var_name):
-        if self.data_dict is not None and name in self.data_dict:
+        if self.data_dict is not None and name in self.data_dict and self.resume:
             value = self.data_dict[name][idx]
         else:
             value = initial_value
